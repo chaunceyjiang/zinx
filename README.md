dd01c2a8e3eebc6b4cece0627d4bfd0d901b60b0
chaunceyjiang <chaunceyjiang@gmail.com>
2019-10-25 09:59:18 +0800

    初始仓库

---
+++ README.md
@@ -0,0 +1,3 @@
+# tcp 服务框架
+-----
+资料 https://legacy.gitbook.com/book/aceld/zinx/details
---
+++ znet/server.go
@@ -0,0 +1,8 @@
+package znet
+
+type Server struct {
+	Name string // 服务器名称
+	IPVersion string // ip版本
+	IP string // 服务器绑定的地址
+	Port int
+}
