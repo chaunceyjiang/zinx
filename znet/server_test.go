bb97090409b0cad4c1cb3ae3d1956186516ef74f
chaunceyjiang <chaunceyjiang@gmail.com>
2019-10-25 11:24:59 +0800

    完成 server的基础封装

--- znet/server_test.go
+++ znet/server_test.go
@@ -5,17 +5,21 @@ import (
 	"github.com/stretchr/testify/assert"
 	"net"
 	"testing"
+	"time"
 	"zinx/utils"
 )
 
 func TestServer(t *testing.T) {
 	server := NewServer("test")
-	server.Serve()
-
+	go server.Serve()
+	time.Sleep(1 * time.Second)
 	addr, _ := net.ResolveTCPAddr(utils.DEFAULT_IP_VERSION, fmt.Sprintf("%s:%d", utils.DEFAULT_IP, utils.DEFAULT_PORT))
 	conn, err := net.DialTCP(utils.DEFAULT_IP_VERSION, nil, addr)
 	assert.Equal(t, nil, err, "dial tcp")
-	_, err = conn.Write([]byte("你好"))
+	_, err = conn.Write([]byte("hello world"))
 	assert.Equal(t, nil, err, "write tcp")
+	buf:=make([]byte,512)
+	n,err:=conn.Read(buf)
+	assert.Equal(t,"hello world",string(buf[:n]),string(buf[:n]))
 
 }
